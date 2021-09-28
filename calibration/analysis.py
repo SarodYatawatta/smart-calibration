cbe18f2a9448886d903958e4ad7c7d38413d9419
Sarod Yatawatta <sarod@dop373.nfra.nl>
2021-09-28 15:54:02 +0200

    parallelize influence func calculation

--- calibration/analysis.py
+++ calibration/analysis.py
@@ -1,11 +1,20 @@
-import math
+import math,sys,uuid
 import numpy as np
 import numpy.matlib
+from multiprocessing import Pool
 from calibration_tools import *
 
 
-def analysis_uvwdir_loop(skymodel,clusterfile,uvwfile,rhofile,solutionsfile,alpha):
+def globalize(func):
+  def result(*args, **kwargs):
+    return func(*args, **kwargs)
+  result.__name__ = result.__qualname__ = uuid.uuid4().hex
+  setattr(sys.modules[result.__module__], result.__name__, result)
+  return result
+
+def analysis_uvwdir_loop(skymodel,clusterfile,uvwfile,rhofile,solutionsfile,alpha,Nparallel=4):
     # alpha: spatial constraint regularization parameter
+    # Nparallel=number of parallel jobs to use
     # stations
     N=62
     #GG N=61
@@ -77,55 +86,61 @@ def analysis_uvwdir_loop(skymodel,clusterfile,uvwfile,rhofile,solutionsfile,alph
        Hadd[ci]=0.5*rho[ci]*np.kron(np.eye(2),np.matmul(FF,np.eye(2*N)+np.matmul(np.linalg.pinv(np.eye(2*N)-FF),FF)))
 
 ############################# loop over timeslots
-    ts=0
-    for ncal in range(Ts):
-     R=np.zeros((2*B*T,2),dtype=np.csingle)
-     R[0:2*B*T:2,0]=XX[ts*B:ts*B+B*T]
-     R[0:2*B*T:2,1]=XY[ts*B:ts*B+B*T]
-     R[1:2*B*T:2,0]=YX[ts*B:ts*B+B*T]
-     R[1:2*B*T:2,1]=YY[ts*B:ts*B+B*T]
-    
-     # D_Jgrad K x 4Nx4N tensor
-     H=Hessianres(R,Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N)
-     H+=Hadd
-    
-     # set to zero
-     XX[ts*B:ts*B+B*T]=0
-     XY[ts*B:ts*B+B*T]=0
-     YX[ts*B:ts*B+B*T]=0
-     YY[ts*B:ts*B+B*T]=0
-    
-     if loop_in_r:
-       for r in range(8):
-         # dJ: K x 4NxB tensor
-         dJ=Dsolutions(Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N,H,r)
-         # dR: 4B x B (sum up all K)
-         dR=Dresiduals(Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N,dJ,0,r) # 0 for not adding I to dR
-         # find mean value over columns
-         dR11=np.mean(dR[0:4*B:4],axis=0)
-         dR11=np.squeeze(np.matlib.repmat(dR11,1,T))
-         XX[ts*B:ts*B+B*T] +=dR11
-         dR11=np.mean(dR[3:4*B:4],axis=0)
-         dR11=np.squeeze(np.matlib.repmat(dR11,1,T))
-         YY[ts*B:ts*B+B*T] +=dR11
-     else:
-       # dJ: 8 x K x 4NxB tensor
-       dJ=Dsolutions_r(Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N,H)
-       # dR: 8 x 4B x B (sum up all K)
-       dR=Dresiduals_r(Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N,dJ,0) # 0 for not adding I to dR
-       # find mean value over columns
-       for r in range(8):
-         dR11=np.mean(dR[r,0:4*B:4],axis=0)
-         dR11=np.squeeze(np.matlib.repmat(dR11,1,T))
-         XX[ts*B:ts*B+B*T] +=dR11
-         dR11=np.mean(dR[r,3:4*B:4],axis=0)
-         dR11=np.squeeze(np.matlib.repmat(dR11,1,T))
-         YY[ts*B:ts*B+B*T] +=dR11
-  
-    
-     print('%d %d %d'%(ts,Ts,ncal))
-     ts +=T
+############################# local function
+    @globalize
+    def process_chunk(ncal):
+        ts=ncal*T
+        print('%d %d %d'%(ts,Ts,ncal))
+        R=np.zeros((2*B*T,2),dtype=np.csingle)
+        R[0:2*B*T:2,0]=XX[ts*B:ts*B+B*T]
+        R[0:2*B*T:2,1]=XY[ts*B:ts*B+B*T]
+        R[1:2*B*T:2,0]=YX[ts*B:ts*B+B*T]
+        R[1:2*B*T:2,1]=YY[ts*B:ts*B+B*T]
+       
+        # D_Jgrad K x 4Nx4N tensor
+        H=Hessianres(R,Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N)
+        H+=Hadd
+       
+        # set to zero
+        XX[ts*B:ts*B+B*T]=0
+        XY[ts*B:ts*B+B*T]=0
+        YX[ts*B:ts*B+B*T]=0
+        YY[ts*B:ts*B+B*T]=0
+       
+        if loop_in_r:
+          for r in range(8):
+            # dJ: K x 4NxB tensor
+            dJ=Dsolutions(Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N,H,r)
+            # dR: 4B x B (sum up all K)
+            dR=Dresiduals(Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N,dJ,0,r) # 0 for not adding I to dR
+            # find mean value over columns
+            dR11=np.mean(dR[0:4*B:4],axis=0)
+            dR11=np.squeeze(np.matlib.repmat(dR11,1,T))
+            XX[ts*B:ts*B+B*T] +=dR11
+            dR11=np.mean(dR[3:4*B:4],axis=0)
+            dR11=np.squeeze(np.matlib.repmat(dR11,1,T))
+            YY[ts*B:ts*B+B*T] +=dR11
+        else:
+          # dJ: 8 x K x 4NxB tensor
+          dJ=Dsolutions_r(Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N,H)
+          # dR: 8 x 4B x B (sum up all K)
+          dR=Dresiduals_r(Ct[:,ts*B:ts*B+B*T],J[:,ncal*2*N:ncal*2*N+2*N],N,dJ,0) # 0 for not adding I to dR
+          # find mean value over columns
+          for r in range(8):
+            dR11=np.mean(dR[r,0:4*B:4],axis=0)
+            dR11=np.squeeze(np.matlib.repmat(dR11,1,T))
+            XX[ts*B:ts*B+B*T] +=dR11
+            dR11=np.mean(dR[r,3:4*B:4],axis=0)
+            dR11=np.squeeze(np.matlib.repmat(dR11,1,T))
+            YY[ts*B:ts*B+B*T] +=dR11
+############################# end local function
 ############################# loop over timeslots
+
+    # create pool
+    pool=Pool(Nparallel)
+    pool.map(process_chunk,range(Ts))
+    pool.close()
+    pool.join()
 
     scalefactor=8*(N*(N-1)/2)*T 
     # scale by 8*(N*(N-1)/2)*T    
@@ -135,13 +150,15 @@ def analysis_uvwdir_loop(skymodel,clusterfile,uvwfile,rhofile,solutionsfile,alph
 
 
 if __name__ == '__main__':
-  # args skymodel clusterfile uvwfile rhofile solutionsfile alpha
+  # args skymodel clusterfile uvwfile rhofile solutionsfile alpha parallel_jobs
   import sys
   argc=len(sys.argv)
-  if argc>6:
+  if argc==7:
    analysis_uvwdir_loop(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],float(sys.argv[6]))
+  elif argc==8:
+   analysis_uvwdir_loop(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],float(sys.argv[6]),int(sys.argv[7]))
   else:
-   print("Usage: python %s skymodel clusterfile uvwfile rhofile solutionsfile alpha"%(sys.argv[0]))
+   print("Usage: python %s skymodel clusterfile uvwfile rhofile solutionsfile alpha parallel_jobs"%(sys.argv[0]))
 
   exit()
 
